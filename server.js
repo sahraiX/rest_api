53282db5130001c48873637d06e9bff9543ccecf
Chanannaphat Singkhal <MJU6404101357@mju.ac.th>
2024-02-16 10:38:47 +0700

    book-id

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+const books = require('./db')
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
