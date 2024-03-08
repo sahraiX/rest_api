b4772aaf2aa854c201d7ad4811da4262c31359d1
Chanannaphat Singkhal <MJU6404101357@mju.ac.th>
2024-03-08 15:06:23 +0700

    book

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('Chanannaphat Singkhal 6404101357')
+  res.send(' Chanannaphat Singkhal 6404101357')
 })
 
 const books = require('./db')
